64515685b437683f180a4af2993fa2932386a4b1
Daniel Cook <dfcook@hotmail.com>
2021-06-05 14:33:54 +0100

    Add support for nitrogen instead of feh

--- styli.sh
+++ styli.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 link="https://source.unsplash.com/random/"
+
 if [ -z ${XDG_CONFIG_HOME+x} ]
 then
     XDG_CONFIG_HOME="${HOME}/.config"
@@ -18,6 +19,7 @@ if [ ! -d "${cachedir}" ]
 then
     mkdir -p "${cachedir}"
 fi
+
 reddit(){
     useragent="thevinter"
     timeout=60
@@ -65,8 +67,24 @@ reddit(){
     ext=`echo -n "${target_url##*.}"|cut -d '?' -f 1`
     newname=`echo $target_name | sed "s/^\///;s/\// /g"`_"$subreddit"_$target_id.$ext
     wget -T $timeout -U "$useragent" --no-check-certificate -q -P down -O "${cachedir}/wallpaper.jpg" $target_url &>/dev/null
-
 }
+
+unsplash() {
+  local search="${search// /_}"
+  if [ ! -z $height ] || [ ! -z $width ]; then
+      link="${link}${width}x${height}";
+  else
+      link="${link}1920x1080";
+  fi
+
+  if [ ! -z $search ]
+  then
+      link="${link}/?${search}"
+  fi
+
+  wget -q -O "${cachedir}/wallpaper.jpg" $link
+}
+
 usage(){
     echo "Usage: styli.sh [-s | --search <string>]
                           [-h | --height <height>]
@@ -78,15 +96,110 @@ usage(){
                           [-p | --termcolor]
                           [-d | --directory]
                           [-k | --kde]
+                          [-m | --monitors <monitor count (nitrogen)>]
+                          [-n | --nitrogen]
                           "
     exit 2
 }
+
+pywal_cmd() {
+  if [ $pywal -eq 1 ]; then
+      wal -c
+      wal -i "${cachedir}/wallpaper.jpg" -n
+  fi
+}
+
+nitrogen_cmd() {
+  for ((monitor=0; monitor < $monitors; monitor++))
+  do
+      local nitrogen=(nitrogen --save --head=${monitor})
+
+      if [ ! -z $bgtype ]; then
+          if [ $bgtype == 'bg-center' ]; then
+              nitrogen+=(--set-centered)
+          fi
+          if [ $bgtype == 'bg-fill' ]; then
+              nitrogen+=(--set-zoom-fill)
+          fi
+          if [ $bgtype == 'bg-max' ]; then
+              nitrogen+=(--set-zoom)
+          fi
+          if [ $bgtype == 'bg-scale' ]; then
+              nitrogen+=(--set-scaled)
+          fi
+          if [ $bgtype == 'bg-tile' ]; then
+              nitrogen+=(--set-tiled)
+          fi
+      else
+          nitrogen+=(--set-scaled)
+      fi
+
+      if [ ! -z $custom ]; then
+          nitrogen+=($custom)
+      fi
+
+      if [ ! -z $dir ]; then
+          nitrogen+=(--random $dir)
+      else
+	        nitrogen+=("${cachedir}/wallpaper.jpg")
+      fi
+
+      "${nitrogen[@]}"
+  done
+}
+
+kde_cmd() {
+	cp "${cachedir}/wallpaper.jpg" "${cachedir}/tmp.jpg"
+    qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript "var allDesktops = desktops();print (allDesktops);for (i=0;i<allDesktops.length;i++) {d = allDesktops[i];d.wallpaperPlugin = \"org.kde.image\";d.currentConfigGroup = Array(\"Wallpaper\", \"org.kde.image\", \"General\");d.writeConfig(\"Image\", \"file:${cachedir}/tmp.jpg\")}"
+	qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript "var allDesktops = desktops();print (allDesktops);for (i=0;i<allDesktops.length;i++) {d = allDesktops[i];d.wallpaperPlugin = \"org.kde.image\";d.currentConfigGroup = Array(\"Wallpaper\", \"org.kde.image\", \"General\");d.writeConfig(\"Image\", \"file:${cachedir}/wallpaper.jpg\")}"
+    rm "${cachedir}/tmp.jpg"
+}
+
+feh_cmd() {
+  local feh=(feh)
+  if [ ! -z $bgtype ]; then
+      if [ $bgtype == 'bg-center' ]; then
+          feh+=(--bg-center)
+      fi
+      if [ $bgtype == 'bg-fill' ]; then
+          feh+=(--bg-fill)
+      fi
+      if [ $bgtype == 'bg-max' ]; then
+          feh+=(--bg-max)
+      fi
+      if [ $bgtype == 'bg-scale' ]; then
+          feh+=(--bg-scale)
+      fi
+      if [ $bgtype == 'bg-tile' ]; then
+          feh+=(--bg-tile)
+      fi
+  else
+	  feh+=(--bg-scale)
+  fi
+
+  if [ ! -z $custom ]; then
+      feh+=($custom)
+  fi
+
+  if [ ! -z $dir ]; then
+    feh+=(--randomize $dir)
+  else
+	  feh+=("${cachedir}/wallpaper.jpg")
+  fi
+
+  "${feh[@]}"
+}
+
 pywal=0
 kde=false
-PARSED_ARGUMENTS=$(getopt -a -n $0 -o h:w:s:l:b:r:c:d:pk --long search:,height:,width:,fehbg:,fehopt:,subreddit:,directory:,termcolor:,kde -- "$@")
+nitrogen=false
+monitors=1
+
+PARSED_ARGUMENTS=$(getopt -a -n $0 -o h:w:s:l:b:r:c:d:m:pkn --long search:,height:,width:,fehbg:,fehopt:,subreddit:,directory:,monitors:,termcolor:,kde,nitrogen -- "$@")
 VALID_ARGUMENTS=$?
 if [ "$VALID_ARGUMENTS" != "0" ]; then
     usage
+    exit
 fi
 while :
 do
@@ -98,75 +211,30 @@ do
         -l | --link)      link=${2} ; shift 2 ;;
         -r | --subreddit) sub=${2} ; shift 2 ;;
         -c | --fehopt)    custom=${2} ; shift 2 ;;
+        -m | --monitors)  monitors=${2} ; shift 2 ;;
+        -n | --nitrogen)  nitrogen=true ; shift ;;
         -d | --directory) dir=${2} ; shift 2 ;;
         -p | --termcolor) pywal=1 ; shift ;;
-        -k | --kde) kde=true ; shift ;;
+        -k | --kde) 	  kde=true ; shift ;;
         -- | '') shift; break ;;
         *) echo "Unexpected option: $1 - this should not happen." ; usage ;;
     esac
 done
-feh=(feh)
-search="${search// /_}"
-if [ ! -z $bgtype ]; then
-    if [ $bgtype == 'bg-center' ]; then
-        feh+=(--bg-center)
-    fi
-    if [ $bgtype == 'bg-fill' ]; then
-        feh+=(--bg-fill)
-    fi
-    if [ $bgtype == 'bg-max' ]; then
-        feh+=(--bg-max)
-    fi
-    if [ $bgtype == 'bg-scale' ]; then
-        feh+=(--bg-scale)
-    fi
-    if [ $bgtype == 'bg-tile' ]; then
-        feh+=(--bg-tile)
-    fi
-else
-    feh+=(--bg-scale)
-fi
-if [ ! -z $custom ]; then
-    feh+=($custom)
-fi
-if [ ! -z $dir ]; then
-    feh+=(--randomize $dir)
-    "${feh[@]}"
-else
-    if [ $link = "reddit" ] || [ ! -z $sub ]
-    then
-        reddit "$sub"
-        feh+=("${cachedir}/wallpaper.jpg")
-        "${feh[@]}"
-        if [ $pywal -eq 1 ]; then
-            wal -c 
-            wal -i "${cachedir}/wallpaper.jpg" -n
-        fi 
-    else
-        if [ ! -z $height ] || [ ! -z $width ]; then
-            link="${link}${width}x${height}";
-        else
-            link="${link}1920x1080";
-        fi
-        if [ ! -z $search ]
-        then
-            link="${link}/?${search}"
-        fi
-        wget -q -O "${cachedir}/wallpaper.jpg" "${link}"
 
-        if [ $kde = true ]; then
-            cp "${cachedir}/wallpaper.jpg" "${cachedir}/tmp.jpg"
-            qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript "var allDesktops = desktops();print (allDesktops);for (i=0;i<allDesktops.length;i++) {d = allDesktops[i];d.wallpaperPlugin = \"org.kde.image\";d.currentConfigGroup = Array(\"Wallpaper\", \"org.kde.image\", \"General\");d.writeConfig(\"Image\", \"file:${cachedir}/tmp.jpg\")}"
-            qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript "var allDesktops = desktops();print (allDesktops);for (i=0;i<allDesktops.length;i++) {d = allDesktops[i];d.wallpaperPlugin = \"org.kde.image\";d.currentConfigGroup = Array(\"Wallpaper\", \"org.kde.image\", \"General\");d.writeConfig(\"Image\", \"file:${cachedir}/wallpaper.jpg\")}"
-            rm "${cachedir}/tmp.jpg"
-        else
-            feh+=("${cachedir}/wallpaper.jpg")
-            "${feh[@]}"
-        fi
-
-        if [ $pywal -eq 1 ]; then
-            wal -c 
-            wal -i "${cachedir}/wallpaper.jpg" -n
-        fi
-    fi
+if [ -z $dir ]; then
+  if [ $link = "reddit" ] || [ ! -z $sub ]; then
+	  reddit "$sub"
+  else
+	  unsplash
+  fi
 fi
+
+if [ $kde = true ]; then
+	kde_cmd
+elif [ $nitrogen = true ]; then
+	nitrogen_cmd
+else
+	feh_cmd
+fi
+
+pywal_cmd
