d71607e8786df8875ce03417df6350255d223bb1
Andrii Maksymets <maksimetsa@gmail.com>
2021-08-14 12:20:46 +0700

    upd

--- styli.sh
+++ styli.sh
@@ -175,48 +175,48 @@ wget -T $timeout -U "$useragent" --no-check-certificate -q -P down -O ${wallpape
 }
 
 unsplash() {
-local search="${search// /_}"
-if [ ! -z $height ] || [ ! -z $width ]; then
-    link="${link}${width}x${height}";
-else
-    link="${link}1920x1080";
-fi
-
-if [ ! -z $search ]; then
-    link="${link}/?${search}"
-fi
-
-wget -q -O ${wallpaper} $link
+    local search="${search// /_}"
+    if [ ! -z $height ] || [ ! -z $width ]; then
+        link="${link}${width}x${height}";
+    else
+        link="${link}1920x1080";
+    fi
+    
+    if [ ! -z $search ]; then
+        link="${link}/?${search}"
+    fi
+    
+    wget -q -O ${wallpaper} $link
 }
 
 deviantart(){
-client_id=16531
-client_secret=68c00f3d0ceab95b0fac638b33a3368e
-payload="grant_type=client_credentials&client_id=${client_id}&client_secret=${client_secret}"
-access_token=`curl --silent -d $payload https://www.deviantart.com/oauth2/token | jq -r '.access_token'`
-if [ ! -z $1 ]; then
-    artist=$1
-    url="https://www.deviantart.com/api/v1/oauth2/gallery/?username=${artist}&mode=popular&limit=24"
-elif [ ! -z $search ]; then
-    [[ "$search" =~ ^(tag:)(.*)$ ]] && tag=${BASH_REMATCH[2]}
-    if [ ! -z $tag ]; then
-        url="https://www.deviantart.com/api/v1/oauth2/browse/tags?tag=$tag&offset=${RANDOM:0:2}&limit=24"
+    client_id=16531
+    client_secret=68c00f3d0ceab95b0fac638b33a3368e
+    payload="grant_type=client_credentials&client_id=${client_id}&client_secret=${client_secret}"
+    access_token=`curl --silent -d $payload https://www.deviantart.com/oauth2/token | jq -r '.access_token'`
+    if [ ! -z $1 ]; then
+        artist=$1
+        url="https://www.deviantart.com/api/v1/oauth2/gallery/?username=${artist}&mode=popular&limit=24"
+    elif [ ! -z $search ]; then
+        [[ "$search" =~ ^(tag:)(.*)$ ]] && tag=${BASH_REMATCH[2]}
+        if [ ! -z $tag ]; then
+            url="https://www.deviantart.com/api/v1/oauth2/browse/tags?tag=$tag&offset=${RANDOM:0:2}&limit=24"
+        else
+            url="https://www.deviantart.com/api/v1/oauth2/browse/popular?q=$search&limit=24&timerange=1month"
+        fi
     else
-        url="https://www.deviantart.com/api/v1/oauth2/browse/popular?q=$search&limit=24&timerange=1month"
+        #url="https://www.deviantart.com/api/v1/oauth2/browse/hot?limit=24&offset=${offset}"
+        topics=( "adoptables" "artisan-crafts" "anthro" "comics" "drawings-and-paintings" "fan-art" "poetry" "stock-images" "sculpture" "science-fiction" "traditional-art" "street-photography" "street-art" "pixel-art" "wallpaper" "digital-art" "photo-manipulation" "science-fiction" "fractal" "game-art" "fantasy" "3d" "drawings-and-paintings" "game-art" )
+        rand=$[$RANDOM % ${#topics[@]}]
+        url="https://www.deviantart.com/api/v1/oauth2/browse/topic?limit=24&topic=${topics[$rand]}"
     fi
-else
-    #url="https://www.deviantart.com/api/v1/oauth2/browse/hot?limit=24&offset=${offset}"
-    topics=( "adoptables" "artisan-crafts" "anthro" "comics" "drawings-and-paintings" "fan-art" "poetry" "stock-images" "sculpture" "science-fiction" "traditional-art" "street-photography" "street-art" "pixel-art" "wallpaper" "digital-art" "photo-manipulation" "science-fiction" "fractal" "game-art" "fantasy" "3d" "drawings-and-paintings" "game-art" )
-    rand=$[$RANDOM % ${#topics[@]}]
-    url="https://www.deviantart.com/api/v1/oauth2/browse/topic?limit=24&topic=${topics[$rand]}"
-fi
-content=`curl --silent -H "Authorization: Bearer ${access_token}" -H "Accept: application/json" -H "Content-Type: application/json" $url`
-urls=$(echo -n $content | jq -r '.results[].content.src')
-arrURLS=($urls)
-size=${#arrURLS[@]}
-idx=$(($RANDOM % $size))
-target_url=${arrURLS[$idx]}
-wget --no-check-certificate -q -P down -O ${wallpaper} $target_url &>/dev/null
+    content=`curl --silent -H "Authorization: Bearer ${access_token}" -H "Accept: application/json" -H "Content-Type: application/json" $url`
+    urls=$(echo -n $content | jq -r '.results[].content.src')
+    arrURLS=($urls)
+    size=${#arrURLS[@]}
+    idx=$(($RANDOM % $size))
+    target_url=${arrURLS[$idx]}
+    wget --no-check-certificate -q -P down -O ${wallpaper} $target_url &>/dev/null
 }
 
 usage(){
@@ -430,9 +430,9 @@ done
 
 if [ ! -z $dir ]; then
     select_random_wallpaper
-    elif [ $link = "reddit" ] || [ ! -z $sub ]; then
+elif [ $link = "reddit" ] || [ ! -z $sub ]; then
     reddit "$sub"
-    elif [ $link = "deviantart" ] || [ ! -z $artist ]; then
+elif [ $link = "deviantart" ] || [ ! -z $artist ]; then
     deviantart "$artist"
 else
     unsplash
@@ -442,13 +442,13 @@ type_check
 
 if [ $kde = true ]; then
     kde_cmd
-    elif [ $xfce = true ]; then
+elif [ $xfce = true ]; then
     xfce_cmd
-    elif [ $gnome = true ]; then
+elif [ $gnome = true ]; then
     gnome_cmd
-    elif [ $nitrogen = true ]; then
+elif [ $nitrogen = true ]; then
     nitrogen_cmd
-    elif [ $sway = true ]; then
+elif [ $sway = true ]; then
     sway_cmd
 else
     feh_cmd
